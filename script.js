3e556c6a441775bdca87e6393104a2a138cf2796
Vashu Nayak <97188321+Vashu-Nayak@users.noreply.github.com>
2024-11-12 15:19:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,50 @@
 function convertToRoman(num) {
-  	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
-    };
+  	// const obj = {
+   //    0:['M',1000], 
+   //    1:['D', 500], 
+   //    2:['C', 100], 
+   //    3:['L', 50], 
+   //    4:['X', 10], 
+   //    5:['V', 5], 
+   //    6:['I', 1]
+   //  };
 
   //your code here
+	if (num < 1 || num > 10000) {
+        return "Input must be between 1 and 3999";
+    }
+
+    const romanNumerals = [
+        { value: 1000, numeral: 'M' },
+        { value: 900, numeral: 'CM' },
+        { value: 500, numeral: 'D' },
+        { value: 400, numeral: 'CD' },
+        { value: 100, numeral: 'C' },
+        { value: 90, numeral: 'XC' },
+        { value: 50, numeral: 'L' },
+        { value: 40, numeral: 'XL' },
+        { value: 10, numeral: 'X' },
+        { value: 9, numeral: 'IX' },
+        { value: 5, numeral: 'V' },
+        { value: 4, numeral: 'IV' },
+        { value: 1, numeral: 'I' },
+    ];
+
+    let result = '';
+    
+    for (const { value, numeral } of romanNumerals) {
+        while (num >= value) {
+            result += numeral;
+            num -= value;
+        }
+    }
+
+    return result;
 
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-// console.log(convertToRoman(36));
+console.log(convertToRoman(36));
 
 
 
